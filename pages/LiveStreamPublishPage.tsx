74cb06fba9095d707e90ffe288d56a1b062a6103
Jai Saravanan <jaisaravanan.cs@gmail.com>
2023-12-19 23:06:43 +0530

    Base code of react native

---
+++ pages/LiveStreamPublishPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function LiveStreamPublishPage(): React.JSX.Element {
+  return (
+    <View>
+      <Text>LiveStreamPublishPage</Text>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({});
+
+export default LiveStreamPublishPage;
---
+++ pages/LiveStreamViewerPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+function LiveStreamViewerPage(): React.JSX.Element {
+  return (
+    <View>
+      <Text>LiveStreamViewerPage</Text>
+    </View>
+  );
+}
+
+// const styles = StyleSheet.create({});
+
+export default LiveStreamViewerPage;
