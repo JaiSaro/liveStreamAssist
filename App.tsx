01ae47feb4bfabfb3bc2fbf008f3cb8466107449
Jai Saravanan <jaisaravanan.cs@gmail.com>
2024-01-08 09:48:47 +0530

    commented the debug code.

--- App.tsx
+++ App.tsx
@@ -10,9 +10,13 @@ import {
   MD3LightTheme as DefaultTheme,
   PaperProvider,
   Text,
+  // Dialog,
+  // Portal,
 } from 'react-native-paper';
 import {PostApiMethod} from './utils/AxiosHelper';
 import AppSnackbar from './components/AppSnackbar';
+
+// import {ScrollView} from 'react-native';
 
 const Stack = createNativeStackNavigator();
 const theme = {
@@ -24,6 +28,9 @@ function App(): React.JSX.Element {
     show: boolean;
     content: string;
   }>({show: false, content: ''});
+  // const [visible, setVisible] = React.useState(false);
+  // const [errorMessage, setErrorMessage] = React.useState('');
+  // const hideDialog = () => setVisible(false);
 
   const authenticateAntMedia = React.useCallback(() => {
     PostApiMethod('users/authenticate', {
@@ -41,7 +48,9 @@ function App(): React.JSX.Element {
         });
       })
       .catch(function (error: any) {
+        // setErrorMessage(error.message);
         console.log(error.message);
+        // setVisible(true);
         setSnackDetails({
           ...{
             show: true,
@@ -81,6 +90,18 @@ function App(): React.JSX.Element {
             component={LiveStreamViewerPage}
           />
         </Stack.Navigator>
+        {/* <Portal>
+          <Dialog visible={visible} onDismiss={hideDialog}>
+            <Dialog.ScrollArea>
+              <ScrollView contentContainerStyle={{paddingHorizontal: 24}}>
+                <Text>Email: {process.env.REACT_APP_USER_EMAIL}</Text>
+                <Text>User Type: {process.env.REACT_APP_USER_TYPE_KEY}</Text>
+                <Text>Password: {process.env.REACT_APP_HASH_KEY}</Text>
+                <Text>Error Message: {errorMessage}</Text>
+              </ScrollView>
+            </Dialog.ScrollArea>
+          </Dialog>
+        </Portal> */}
         <AppSnackbar
           showSnackBar={snackDetails.show}
           snackBarContent={snackDetails.content}
