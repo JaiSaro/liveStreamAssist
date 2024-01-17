6b757aff51d934ea5ff4733fb5c46c3069b2559b
Jai Saravanan <jaisaravanan.cs@gmail.com>
2024-01-17 23:28:58 +0530

    fixed the lint error in VodPlayer.tsx

--- pages/VodPlayer.tsx
+++ pages/VodPlayer.tsx
@@ -1,10 +1,10 @@
-import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Button, Text } from 'react-native-paper';
-import Video from 'react-native-video';
-import Clipboard from '@react-native-clipboard/clipboard';
+import React from "react";
+import { StyleSheet } from "react-native";
+import { Button, Text } from "react-native-paper";
+import Video from "react-native-video";
+import Clipboard from "@react-native-clipboard/clipboard";
 
-function VodPlayer({ route, navigation }: any) {
+function VodPlayer({ route }: any) {
   const routeParamsValue: any = route.params;
   console.log(routeParamsValue.streamId);
   const videoRef = React.useRef<any>(null);
@@ -28,20 +28,20 @@ function VodPlayer({ route, navigation }: any) {
         // Can be a URL or a local file.
         source={{
           uri: `https://ams-11462.antmedia.cloud:5443/WebRTCAppEE/play.html?id=streams/${routeParamsValue.vodName}`,
-          type: 'mp4',
+          type: "mp4",
           headers: {
-            range: 'bytes=0-',
+            range: "bytes=0-",
           },
         }}
         // Store reference
         ref={videoRef}
         // Callback when remote video is buffering
         onBuffer={() => {
-          console.log('On Buffer');
+          console.log("On Buffer");
         }}
         // Callback when video cannot be loaded
         onError={(error) => {
-          console.log('On Error', error);
+          console.log("On Error", error);
         }}
         style={styles.backgroundVideo}
       />
@@ -64,7 +64,7 @@ function VodPlayer({ route, navigation }: any) {
 // Later on in your styles..
 var styles = StyleSheet.create({
   backgroundVideo: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
     left: 0,
     bottom: 0,
@@ -77,7 +77,7 @@ var styles = StyleSheet.create({
   },
   copiedText: {
     marginTop: 10,
-    color: 'red',
+    color: "red",
   },
 });
 
